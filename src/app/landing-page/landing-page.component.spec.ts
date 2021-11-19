fe47d70ce7c096b0f9a67264c27a05eaf9cc7fc5
ucortes227 <ucortes227@gmail.com>
2021-11-19 01:48:33 -0600

    landing-page v1 #1

--- src/app/landing-page/landing-page.component.spec.ts
+++ src/app/landing-page/landing-page.component.spec.ts
@@ -19,7 +19,37 @@ describe('LandingPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render Ulises Cortes San Miguel', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.menu')?.textContent).toContain('Ulises Cortes San Miguel  S19004858');
+  });
+
+  it('should render S19004858', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.menu')?.textContent).toContain('Ulises Cortes San Miguel  S19004858');
+  });
+
+  it('should render the seccion Definición de la ley de Ohm', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content h3')?.textContent).toContain('Definición de la ley de Ohm');
+  });
+
+  it('should render the formula', () => {
+    const fixture = TestBed.createComponent(LandingPageComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span h4')?.textContent).toContain('V = R • I');
   });
 });
